4039c4291eef461614a32c5bbc1e78bcb8229a21
emarkle <40635541+emarkle@users.noreply.github.com>
2024-01-19 14:31:32 -0500

    Update git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -4,32 +4,13 @@ set -e
 
 SOURCE_REPO=$1
 SOURCE_BRANCH=$2
-DESTINATION_REPO=$3
-DESTINATION_BRANCH=$4
-DEVELOPMENT_REPO=$5
-DEVELOPMENT_BRANCH=$6
+SOURCE_SSH_PRIVATE_KEY=$3
 
-if ! echo $SOURCE_REPO | grep -Eq ':|@|\.git\/?$'; then
-  if [[ -n "$SSH_PRIVATE_KEY" || -n "$SOURCE_SSH_PRIVATE_KEY" ]]; then
-    SOURCE_REPO="git@github.com:${SOURCE_REPO}.git"
-    GIT_SSH_COMMAND="ssh -v"
-  else
-    SOURCE_REPO="https://github.com/${SOURCE_REPO}.git"
-  fi
-fi
 
-if ! echo $DESTINATION_REPO | grep -Eq ':|@|\.git\/?$'; then
-  if [[ -n "$SSH_PRIVATE_KEY" || -n "$DESTINATION_SSH_PRIVATE_KEY" ]]; then
-    DESTINATION_REPO="git@github.com:${DESTINATION_REPO}.git"
-    GIT_SSH_COMMAND="ssh -v"
-  else
-    DESTINATION_REPO="https://github.com/${DESTINATION_REPO}.git"
-  fi
-fi
 
 echo "SOURCE=$SOURCE_REPO:$SOURCE_BRANCH"
-echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
-echo "DEVELOPMENT=$DEVELOPMENT_REPO:$DEVELOPMENT_BRANCH"
+#echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
+#echo "DEVELOPMENT=$DEVELOPMENT_REPO:$DEVELOPMENT_BRANCH"
 
 if [[ -n "$SOURCE_SSH_PRIVATE_KEY" ]]; then
   # Clone using source ssh key if provided
@@ -38,29 +19,29 @@ if [[ -n "$SOURCE_SSH_PRIVATE_KEY" ]]; then
   echo 'echo $SOURCE_SSH_PASSPHRASE' >> ~/.ssh_askpass && chmod +x ~/.ssh_askpass
   echo "$(~/.ssh_askpass)"
   echo "$SOURCE_SSH_PRIVATE_KEY" | tr -d '\r' | DISPLAY=None SSH_ASKPASS=~/.ssh_askpass ssh-add - >/dev/null
-  git clone -c core.sshCommand="/usr/bin/ssh -i ~/.ssh/src_rsa" "$SOURCE_REPO" /root/source --origin source && cd /root/source
+  #git clone -c core.sshCommand="/usr/bin/ssh -i ~/.ssh/src_rsa" "$SOURCE_REPO" /root/source --origin source && cd /root/source
 else
   git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
 fi
 
-git remote add destination "$DESTINATION_REPO"
-git remote add development "$DEVELOPMENT_REPO"
+#git remote add destination "$DESTINATION_REPO"
+#git remote add development "$DEVELOPMENT_REPO"
 
 # Pull all branches references down locally so subsequent commands can see them
-git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
+#git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
 
 # Print out all branches
-git --no-pager branch -a -vv
+#git --no-pager branch -a -vv
 
 if [[ -n "$DESTINATION_SSH_PRIVATE_KEY" ]]; then
   # Push using destination ssh key if provided
-  git config --local core.sshCommand "/usr/bin/ssh -i ~/.ssh/dst_rsa"
+ # git config --local core.sshCommand "/usr/bin/ssh -i ~/.ssh/dst_rsa"
 fi
 
-git push destination --all 
-git push destination -f --tags 
+#git push destination --all 
+#git push destination -f --tags 
 
 
-git push development "master:master" -f
-git push development -f --tags
+#git push development "master:master" -f
+#git push development -f --tags
 
